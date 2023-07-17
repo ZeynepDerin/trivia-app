b1d314b3a570675929ac6c255f7ac3aa31103440
ZeynepDerin <zozturk220@gmail.com>
2023-07-17 17:24:12 +0200

    new features

---
+++ gamepage/gamepage.js
@@ -0,0 +1,100 @@
+
+/* gamepage.js
+GAME PAGE */
+
+  const gameContainer = document.getElementById("game")
+  let scoreEl = document.getElementById("score")
+  let score = 0
+  
+  const categories = [
+    {name: "Science & Nature",
+    id:"17"},
+    {name: "Geography",
+    id:"22"}, 
+    {name: "Art",
+    id:"25"}]
+
+  const levels = ["easy", "medium", "hard"]
+  
+  function addGenre(category){
+    const column = document.createElement("div")
+    gameContainer.append(column)
+    column.classList.add("column-style")
+    const columnName = document.createElement("h4")
+    column.append(columnName)
+    columnName.textContent = category.name
+    columnName.setAttribute("id", "genre-name")
+  
+   levels.forEach( level => {
+      const card = document.createElement("div")
+      card.classList.add("card")
+      column.append(card)
+  
+      fetch(`https://opentdb.com/api.php?amount=1&category=${category.id}&difficulty=${level}&type=boolean`)
+      .then(result => result.json())
+      .then(data => {
+        card.setAttribute("data-question", data.results[0].question)
+        card.setAttribute("data-correct_answer", data.results[0].correct_answer)
+        card.setAttribute("data-score", level === "easy" ? "100" : level === "medium" ? "200" : "300")
+        card.innerText = level === "easy" ? "100" : level === "medium" ? "200" : "300"
+        
+      })
+      card.addEventListener("click", flipTheCard)
+  
+    })
+  
+  }
+  
+  categories.forEach(category => addGenre(category))
+  
+  
+  function flipTheCard() {
+    
+    const trueButton = document.createElement("button")
+    const falseButton = document.createElement("button")
+    const btnDisplay = document.createElement("div")
+   
+    trueButton.innerText = "True"
+    falseButton.innerText = "False"
+    trueButton.classList.add("true-btn")
+    falseButton.classList.add("false-btn")
+    trueButton.addEventListener("click", getAnswer)
+    falseButton.addEventListener("click", getAnswer)
+    this.innerHTML = this.dataset.question
+    this.append(btnDisplay)
+    btnDisplay.append(trueButton, falseButton)
+    this.classList.add("flipped-card")
+    const allCards = Array.from(document.querySelectorAll(".card"))
+    allCards.forEach(card => card.removeEventListener("click", flipTheCard))
+  
+  }
+  
+  function getAnswer(){
+  
+    const cardEl = this.parentElement.parentElement
+    cardEl.classList.remove("flipped-card")
+    cardEl.classList.remove("card")
+    cardEl.classList.add("answered-card")
+    
+    if(this.innerText === cardEl.dataset.correct_answer){
+      score = score + parseInt(cardEl.dataset.score)
+      scoreEl.innerText = "Your Score: " + score
+      while(cardEl.firstChild){
+        cardEl.removeChild(cardEl.lastChild)
+      }
+      
+      
+    } else {
+      while (cardEl.firstChild) {
+        cardEl.removeChild(cardEl.lastChild);
+      }
+    }
+  
+   const allCards = document.querySelectorAll(".card")
+   allCards.forEach(card => card.addEventListener("click", flipTheCard))
+    
+  }
+
+ 
+
+ 
--- index.js
+++ index.js
@@ -1,89 +1,89 @@
-const gameContainer = document.getElementById("game")
-let scoreEl = document.getElementById("score")
-let score = 0
 
-const categories = [
-  {name: "Science & Nature",
-  id:"17"},
-  {name: "Geography",
-  id:"22"}, 
-  {name: "Art",
-  id:"25"}]
-const levels = ["easy", "medium", "hard"]
+/* index.js
+HOME PAGE */
 
-function addGenre(category){
-  const column = document.createElement("div")
-  gameContainer.append(column)
-  column.classList.add("column-style")
 
- levels.forEach( level => {
-    const card = document.createElement("div")
-    card.classList.add("card")
-    column.append(card)
+const chooseCategoriesContainer = document.getElementById("choose-categories-container")
+const setYourChoice = document.getElementById("set-the-categories")
+const startGame = document.getElementById("start-game")
+const categoryDroplistOne =document.getElementById("category-droplist-one")
+const categoryDroplistTwo =document.getElementById("category-droplist-two")
+const categoryDroplistThree =document.getElementById("category-droplist-three")
 
-    fetch(`https://opentdb.com/api.php?amount=1&category=${category}&difficulty=${level}&type=boolean`)
-    .then(result => result.json())
-    .then(data => {
-      card.setAttribute("data-question", data.results[0].question)
-      card.setAttribute("data-correct_answer", data.results[0].correct_answer)
-      card.setAttribute("data-score", level === "easy" ? "100" : level === "medium" ? "200" : "300")
-      card.innerText = level === "easy" ? "100" : level === "medium" ? "200" : "300"
-      
+const dropLists = [categoryDroplistOne, categoryDroplistTwo, categoryDroplistThree]
+
+  const categoriesToChooseFrom = [
+    {name: "General Knowledge",
+    id:"9"},
+    {name: "Science & Nature",
+    id:"17"}, 
+    {name: "Science: Mathematics",
+    id:"19"}, 
+    {name: "Mythology",
+    id:"20"}, 
+    {name: "Geography",
+    id:"22"}, 
+    {name: "History",
+    id:"23"}, 
+    {name: "Art",
+    id:"25"}, 
+    {name: "Animals",
+    id:"27"}, 
+    {name: "Sports",
+    id:"21"}, 
+    {name: "Science: Gadgets",
+    id:"30"}]
+  
+  
+function createDropLists(list){
+    categoriesToChooseFrom.forEach (item => {
+      list.innerHTML += `
+      <option value="${item.id}" name="${item.name}"> ${item.name} </option> ` 
     })
-    card.addEventListener("click", flipTheCard)
+  }
+  
+dropLists.forEach(list => createDropLists(list))
 
+
+startGame.addEventListener("click", disableStartBtn)
+function disableStartBtn(e){
+  e.preventDefault()
+}
+
+
+
+  let filteredCategories =[]
+    setYourChoice.addEventListener("click", function(){  
+    filteredCategories =[]
+  
+    dropLists.forEach(list => chosenOnes(list))
+  
+    function chosenOnes(list){ categoriesToChooseFrom.forEach((item) => {
+      if(item.id === list.value){
+        filteredCategories.push(item) 
+      }  
+      })}
+  
+    startGame.classList.remove("start-game-disabled")
+    startGame.classList.add("start-game")
+    startGame.removeEventListener("click", disableStartBtn)
+  
+    console.log(filteredCategories)
+  
   })
 
-}
-
-categories.forEach(category => addGenre(category.id))
 
 
-function flipTheCard() {
+
+
+
   
-  const trueButton = document.createElement("button")
-  const falseButton = document.createElement("button")
- 
-  trueButton.innerText = "True"
-  falseButton.innerText = "False"
-  trueButton.classList.add("true-btn")
-  falseButton.classList.add("false-btn")
-  trueButton.addEventListener("click", getAnswer)
-  falseButton.addEventListener("click", getAnswer)
-  this.innerHTML = this.dataset.question
-  this.append(trueButton, falseButton)
-  this.classList.add("flipped-card")
-  const allCards = Array.from(document.querySelectorAll(".card"))
-  allCards.forEach(card => card.removeEventListener("click", flipTheCard))
 
-}
 
-function getAnswer(){
 
-  const cardEl = this.parentElement
-  cardEl.classList.remove("flipped-card")
-  cardEl.classList.remove("card")
-  cardEl.classList.add("answered-card")
-  
-  if(this.innerText === cardEl.dataset.correct_answer){
-    score = score + parseInt(cardEl.dataset.score)
-    scoreEl.innerText = "Your Score: " + score
-    while(cardEl.firstChild){
-      cardEl.removeChild(cardEl.lastChild)
-    }
-    
-    
-  } else {
-    while (cardEl.firstChild) {
-      cardEl.removeChild(cardEl.lastChild);
-    }
-  }
 
- const allCards = document.querySelectorAll(".card")
- allCards.forEach(card => card.addEventListener("click", flipTheCard))
-  
-  
-}
+
+
 
 
 
